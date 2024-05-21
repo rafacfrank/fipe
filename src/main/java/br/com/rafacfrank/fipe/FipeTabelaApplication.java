32065a4aa62ba86c763131f5d89ee31631b7fae3
Rafael Frank <rafacfrank@gmail.com>
2024-05-20 22:29:43 -0300

    Initial commit

---
+++ src/main/java/br/com/rafacfrank/fipe/FipeTabelaApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafacfrank.fipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeTabelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeTabelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafacfrank/fipe/FipeTabelaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafacfrank.fipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeTabelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
